46d9aebcad7a34bc5b9a3ece0407adb0a305b864
jasonChiu <jason1122g@gmail.com>
2015-05-25 13:49:45 +0800

    fix configuration property name

--- config-hikaricp/src/main/java/tw/edu/ncu/cc/springboot/config/hikaricp/HikariCPConfig.java
+++ config-hikaricp/src/main/java/tw/edu/ncu/cc/springboot/config/hikaricp/HikariCPConfig.java
@@ -5,44 +5,17 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import java.util.Properties;
 
 
-@ConfigurationProperties( "spring.datasource" )
+@ConfigurationProperties( "spring" )
 public class HikariCPConfig {
 
-    private String url;
-    private String username;
-    private String password;
-    private Properties hikariProperties;
+    private Properties hikariDatasource;
 
-    public String getUrl() {
-        return url;
+    public Properties getHikariDatasource() {
+        return hikariDatasource;
     }
 
-    public void setUrl( String url ) {
-        this.url = url;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername( String username ) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword( String password ) {
-        this.password = password;
-    }
-
-    public Properties getHikariProperties() {
-        return hikariProperties;
-    }
-
-    public void setHikariProperties( Properties hikariProperties ) {
-        this.hikariProperties = hikariProperties;
+    public void setHikariDatasource( Properties hikariDatasource ) {
+        this.hikariDatasource = hikariDatasource;
     }
 
 }
